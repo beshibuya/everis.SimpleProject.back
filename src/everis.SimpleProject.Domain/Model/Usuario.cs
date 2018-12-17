1aec8fd717d01b5db61d55da47b71ae2af838a8f
beijishi <beijishi@SAO-6X5W9R2>
2018-12-17 15:23:20 -0200

    Exemplo do Dominio

---
+++ src/everis.SimpleProject.Domain/Model/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace everis.SimpleProject.Domain.Model
+{
+    public class Usuario
+    {
+        public Guid UsuarioId { get; set; }
+        public string NomeUsuario { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public bool SenhaTemporaria { get; set; }
+        public DateTime DataCadastro { get; set; }
+        public DateTime? DataDesativacao { get; set; }
+    }
+}
---
+++ src/everis.SimpleProject.Domain/Service/IUsuariosService.cs
@@ -0,0 +1,18 @@
+﻿using everis.SimpleProject.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace everis.SimpleProject.Domain.Service
+{
+    interface IUsuariosService : IDisposable
+    {
+        Usuario Adicionar(Usuario obj);
+        Usuario ObterPorId(int id);
+        IEnumerable<Usuario> ObterTodos();
+        Usuario Atualizar(Usuario obj);
+        void Remover(int id);
+        IEnumerable<Usuario> Buscar(Expression<Func<Usuario, bool>> predicate);
+        int SaveChanges();
+    }
+}
