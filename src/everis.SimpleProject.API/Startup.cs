ceabcc6184c5d53c72c619f928f495b25b59b745
Laerte Uliam <laerte.uliam@gmail.com>
2018-12-17 14:48:34 -0200

    Configuração Swagger

--- src/everis.SimpleProject.API/Startup.cs
+++ src/everis.SimpleProject.API/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace everis.SimpleProject.API
 {
@@ -26,6 +27,11 @@ namespace everis.SimpleProject.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = Configuration["ApiConfigs:Title"], Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -40,8 +46,20 @@ namespace everis.SimpleProject.API
                 app.UseHsts();
             }
 
+            ConfigureSwagger(app);
+
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        private static void ConfigureSwagger(IApplicationBuilder app)
+        {
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
         }
     }
 }
