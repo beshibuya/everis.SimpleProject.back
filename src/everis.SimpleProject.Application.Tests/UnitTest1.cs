99b53cc338d40a50ad92554101b5826a3105c18b
beijishi <beijishi@SAO-6X5W9R2>
2018-12-17 14:28:40 -0200

    inicial

---
+++ src/everis.SimpleProject.Application.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace everis.SimpleProject.Application.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ src/everis.SimpleProject.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace everis.SimpleProject.Application
+{
+    public class Class1
+    {
+    }
+}
