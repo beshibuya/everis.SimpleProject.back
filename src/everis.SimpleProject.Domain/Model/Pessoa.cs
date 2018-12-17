f796010575bdfd2bc437189a53f5c3cc8e3dd531
Ricardo Grieco <ricardogrieco@hotmail.com>
2018-12-17 17:11:07 -0200

    Model Anexo , Model Pessoa

---
+++ src/everis.SimpleProject.Domain/Model/Pessoa.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace everis.SimpleProject.Domain.Model
+{
+    class Pessoa
+    { 
+        public enum eTipoPessoa
+        {
+            Exemplo1 = 1,
+            Exemplo2 = 2
+        }
+
+        public class Anexo
+        {
+            [Key]
+            [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+            public int IdPessoa { get; set; }
+            [Required]
+            public string Nome { get; set; }
+            [Required]
+            public eTipoPessoa Tipo { get; set; }
+            public string Email { get; set; }
+            public string Documento { get; set; }
+            public long CPF { get; set; }
+            [Required]
+            public bool Ativo { get; set; }
+            public string FotoPath { get; set; }
+            [Required]
+            public int IdEmpresa { get; set; }  // FK 
+            public Empresa Empresa { get; set; } // FK 
+        }
+    }
+}
