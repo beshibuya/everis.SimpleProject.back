6a6132d9615be99c34e86e16cb70fcb68291218c
Yondo-r <yondo.ricardo@gmail.com>
2018-12-18 11:36:00 -0200

    atualizando branch dev

---
+++ src/everis.SimpleProject.Domain/Model/Empresa.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace everis.SimpleProject.Domain.Model
+{
+    class Empresa
+    {
+        public int IdEmpresa { get; set; }
+        public string Nome { get; set; }
+        public short Segmento { get; set; }
+    }
+}
