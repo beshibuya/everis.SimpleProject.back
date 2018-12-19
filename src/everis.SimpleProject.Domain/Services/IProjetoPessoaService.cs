a989083a30cca4a6d7ba15db5fd7e987667c8652
Raoni1984 <raonico@gmail.com>
2018-12-19 16:14:35 -0200

    Created ProjetoPessoaController and IProjetoPessoaService

---
+++ src/everis.SimpleProject.Domain/Services/IProjetoPessoaService.cs
@@ -0,0 +1,14 @@
+﻿using everis.SimpleProject.Domain.Model;
+using System;
+
+namespace everis.SimpleProject.Domain.Service
+{
+    interface IProjetoPessoaService : IDisposable
+    {
+        ProjetoPessoa Adicionar(ProjetoPessoa projetoPessoa);
+        ProjetoPessoa Atualizar(ProjetoPessoa projetoPessoa);
+        ProjetoPessoa Remover(int projetoPessoaId);
+        ProjetoPessoa GetProjetoPessoa(int projetoPessoaId);
+        ProjetoPessoa GetProjetoPessoa(ProjetoPessoa projetoPessoa);
+    }
+}
