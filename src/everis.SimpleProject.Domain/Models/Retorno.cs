a63817d84ba46f532714a020946ac28a7f160c7b
Guilherme Gomes da Silva <guilherme_gs@live.com>
2018-12-20 16:36:28 -0200

    Classe Retorno

---
+++ src/everis.SimpleProject.Domain/Models/Retorno.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace everis.SimpleProject.Domain.Models
+{
+    class Retorno
+    {
+        public Retorno()
+        {
+            Parameters = new Dictionary<string, object>();
+        }
+        public int Codigo { get; set; }
+        public string Mensagem { get; set; }
+        public string Data { get; set; }
+        public new Dictionary<string, object> Parameters { get; set; }
+
+    }
+}
