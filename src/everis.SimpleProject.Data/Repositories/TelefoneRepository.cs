78fe3c4766bfcf150593cee08a88c46bf4b2c7a4
Raoni1984 <raonico@gmail.com>
2018-12-20 16:26:25 -0200

    Adicionado Telefone Repository e Services

---
+++ src/everis.SimpleProject.Data/Repositories/TelefoneRepository.cs
@@ -0,0 +1,12 @@
+﻿using everis.SimpleProject.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace everis.SimpleProject.Data.EF.Repositories
+{
+    public class TelefoneRepository : GenericRepository<Telefone>
+    {
+        public TelefoneRepository(DbContext dbContext) : base(dbContext)
+        {
+        }
+    }
+}
