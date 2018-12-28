8d5ff65d17cb467bee626a8d37c88f009a9e5384
vmonteir <vmonteir@SAO-6H8DCD2>
2018-12-28 15:58:56 -0200

    EsforcoProjeto

---
+++ src/everis.SimpleProject.Application/Services/EsforcoProjetoAppSvcGeneric.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using everis.SimpleProject.Data.EF.Repositories;
+using everis.SimpleProject.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace everis.SimpleProject.Application.Services
+{
+    public class EsforcoProjetoAppSvcGeneric : GenericService<EsforcoProjeto>
+    {
+        public EsforcoProjetoAppSvcGeneric(DbContext context) : base(context)
+        {
+            repository = new GenericRepository<EsforcoProjeto>(context);
+        }
+
+        public override IEnumerable<EsforcoProjeto> BuscarPor(EsforcoProjeto filter)
+        {
+            try
+            {
+                var nomeToFind = filter?.ProjetoPessoa.Pessoa.Nome;
+                var result = repository.BuscarPor(b => b.ProjetoPessoa.Pessoa.Nome.Contains(
+                    string.IsNullOrEmpty(nomeToFind) ? b.ProjetoPessoa.Pessoa.Nome : nomeToFind
+                    ));
+                return result;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
