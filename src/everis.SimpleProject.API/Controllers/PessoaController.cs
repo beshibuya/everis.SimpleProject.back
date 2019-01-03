75f57bce4916a17e79ae91bb042dde6a421a0480
Nicholas Dela Torre <nicholas.delatorre@gmail.com>
2019-01-03 15:16:56 -0200

    Edit PessoaController

--- src/everis.SimpleProject.API/Controllers/PessoaController.cs
+++ src/everis.SimpleProject.API/Controllers/PessoaController.cs
@@ -1,8 +1,50 @@
-﻿using everis.SimpleProject.Domain.Models;
+﻿using everis.SimpleProject.API.ViewModel;
+using everis.SimpleProject.Domain.Models;
+using everis.SimpleProject.Domain.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
 
 namespace everis.SimpleProject.API.Controllers
 {
     public class PessoaController : BaseController<Pessoa>
     {
+
+
+        [HttpPost("[action]")]
+        public ActionResult CriarPessoaColaborador([FromServices]IGenericService<Pessoa> pessoaSvc, [FromServices] IGenericService<Colaborador> colaboradorSvc, [FromBody] PessoaColaborador pcv)
+        {
+            try
+            {
+
+                var novaPessoa = pessoaSvc.Adicionar(pcv.pessoa);
+                pcv.colaborador.PessoaId = novaPessoa.Id;
+                var novoColaborador = colaboradorSvc.Adicionar(pcv.colaborador);
+
+                var pessoaColaborador = new PessoaColaborador
+                {
+                    pessoa = novaPessoa,
+                    colaborador = novoColaborador
+                };
+
+                var retorno = new Retorno()
+                {
+                    Codigo = 200,
+                    Data = pessoaColaborador
+
+                };
+                return Ok(retorno);
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(new Retorno()
+                {
+                    Codigo = 500,
+                    Mensagem = ex.Message
+                });
+            }
+
+        }
+
     }
 }
