5554b084f9b9520f5077247f1c2dd793ff312609
Guilherme Gomes da Silva <guilherme_gs@live.com>
2019-01-21 11:21:56 -0200

    Migration

---
+++ src/everis.SimpleProject.Data/Migrations/20190121123845_merge.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace everis.SimpleProject.Data.EF.Migrations
+{
+    public partial class merge : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Diretoria",
+                table: "Projetos");
+
+            migrationBuilder.RenameColumn(
+                name: "Fase",
+                table: "Projetos",
+                newName: "DiretoriaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projetos_DiretoriaId",
+                table: "Projetos",
+                column: "DiretoriaId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projetos_DiretoriasContratantes_DiretoriaId",
+                table: "Projetos",
+                column: "DiretoriaId",
+                principalTable: "DiretoriasContratantes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projetos_DiretoriasContratantes_DiretoriaId",
+                table: "Projetos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projetos_DiretoriaId",
+                table: "Projetos");
+
+            migrationBuilder.RenameColumn(
+                name: "DiretoriaId",
+                table: "Projetos",
+                newName: "Fase");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Diretoria",
+                table: "Projetos",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
