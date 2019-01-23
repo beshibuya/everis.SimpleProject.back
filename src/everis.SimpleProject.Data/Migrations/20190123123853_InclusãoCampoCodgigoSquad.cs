3d4913642ad5f03fdd580bcb81e3e184fd4837ff
David Rezende Torres <david.r.torres@hotmail.com>
2019-01-23 10:40:39 -0200

    Inclusão campo código squad

---
+++ src/everis.SimpleProject.Data/Migrations/20190123123853_InclusãoCampoCodgigoSquad.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace everis.SimpleProject.Data.EF.Migrations
+{
+    public partial class InclusãoCampoCodgigoSquad : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Codigo",
+                table: "Squads",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Codigo",
+                table: "Squads");
+        }
+    }
+}
